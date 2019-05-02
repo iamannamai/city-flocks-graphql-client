8427bd1282641a78f659a5d5b4bf95987b962cda
Daniel Marquez <DannyBrianMarquez@gmail.com>
2019-05-02 11:13:39 -0400

    feat: add MapView to app

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import { MapView } from 'expo';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
@@ -9,11 +10,21 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+    //   <ScrollView style={styles.container}>
+    //     {/* Go ahead and delete ExpoLinksView and replace it with your
+    //        * content, we just wanted to provide you with some helpful links */}
+    //     <ExpoLinksView />
+	//   </ScrollView>
+	<MapView
+		showsUserLocation={true}
+		style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 40.710000, //40.704540
+          longitude: -74.008000, //-74.009470
+          latitudeDelta: 0.02, // 0.0922
+          longitudeDelta: 0.009, // 0.0421
+        }}
+      />
     );
   }
 }
