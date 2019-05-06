625b660d4c3f56542a65ba1d1f904d858e3cbccf
AbuSufianIslam <thebooming1992@gmail.com>
2019-05-06 17:10:00 -0400

    Up to EventList

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -2,47 +2,119 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import {
-	Text,
-	View
-} from 'react-native';
-
-import {
-	Button
+  Container,
+  Button,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  Icon,
+  H2,
+  List,
+  ListItem,
+  Thumbnail,
+  Left,
+  Right,
+  Content
 } from 'native-base';
 
 import { logout } from '../store';
+const logo = require('../assets/images/avataaars.png')
 
 class UserScreen extends Component {
-	_signOutAsync = async () => {
-		await this.props.logout();
-		this.props.navigation.navigate('Auth');
-	}
+  _signOutAsync = async () => {
+    await this.props.logout();
+    this.props.navigation.navigate('Auth');
+  }
 
-	render() {
-		return (
-			<View>
-				<Button onPress={this._signOutAsync}>
-					<Text>Sign me out :)</Text>
-				</Button>
+  render() {
+    return (
+      <Container>
 
 
-				<Text>{`Welcome Back ${this.props.user.username}`}</Text>
-				<Text>Testing</Text>
-			</View>
-		);
-	}
+        <Card>
+          <CardItem style={{
+            flexDirection: 'column'
+          }}>
+            <Icon name='person' />
+            <H2>{`Welcome Back ${this.props.user.username}`}</H2>
+            <Text>Testing</Text>
+
+          </CardItem>
+          <CardItem style={{ justifyContent: 'center' }}>
+            <Button onPress={this._signOutAsync}>
+              <Text>Sign me out :</Text>
+            </Button>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem header bordered>
+
+            <Text>Events</Text>
+          </CardItem>
+          <CardItem >
+            <Content>
+              <List>
+                <ListItem thumbnail>
+                  <Left>
+                    <Thumbnail square source={logo} />
+                  </Left>
+                  <Body style={{ flex: 1 }}>
+                    <Text>New PLace</Text>
+                    <Text note numberOfLines={1}> Why So Serious!!!!!!</Text>
+                  </Body>
+                  <Right>
+                    <Button transparent>
+                      <Text>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+                <ListItem thumbnail>
+                  <Left>
+                    <Thumbnail square source={logo} />
+                  </Left>
+                  <Body style={{ flex: 1 }}>
+                    <Text>New PLace</Text>
+                    <Text note numberOfLines={1}> Why So Serious!!!!!!</Text>
+                  </Body>
+                  <Right>
+                    <Button transparent>
+                      <Text>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+              </List>
+            </Content>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem header bordered>
+            <Text>Testing</Text>
+          </CardItem>
+          <CardItem >
+
+            {/* <Text>{`Welcome Back ${this.props.user.username}`}</Text> */}
+            <Text>Team</Text>
+          </CardItem>
+        </Card>
+
+      </Container>
+    );
+  }
 }
 
 const mapState = state => {
-	return {
-		user: state.user
-	};
+  return {
+    user: state.user
+  };
 };
 
 const mapDispatch = dispatch => {
-	return {
-		logout: () => dispatch(logout())
-	};
+  return {
+    logout: () => dispatch(logout())
+  };
 };
 
 export default connect(mapState, mapDispatch)(UserScreen);
