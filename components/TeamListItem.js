9ec2b4d35f725bdafc3d4b4bdb61eb9349815000
AbuSufianIslam <thebooming1992@gmail.com>
2019-05-08 13:09:43 -0400

    without redux store

---
+++ components/TeamListItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    Body,
+    Text,
+    ListItem,
+    Left,
+    Thumbnail,
+    Right,
+    Button
+} from 'native-base';
+const avatar = require('../assets/images/avataaars.png');
+export default function TeamListItem(props) {
+    return (
+        <ListItem avatar>
+            <Left>
+                <Thumbnail source={avatar} />
+            </Left>
+            <Body>
+                <Text>
+                    Danny
+		        </Text>
+            </Body>
+            {props.addToTeam ? (
+                <Right>
+                    <Button>
+                        <Text>
+                            Add
+                         </Text>
+                    </Button>
+                </Right>
+            ) : null
+            }
+        </ListItem>
+    )
+}
--- screens/TeamScreen.js
+++ screens/TeamScreen.js
@@ -6,19 +6,83 @@ import {
 	CardItem,
 	Body,
 	Text,
-	Icon
+	Icon,
+	H1,
+	Input,
+	Button,
+	List,
+	ListItem,
+	Left,
+	Thumbnail
 } from 'native-base';
+import TeamListItem from '../components/TeamListItem'
 
-export default class TeamScreen extends Component {
+class TeamScreen extends Component {
 	render() {
 		return (
 			<Content>
+				<H1>
+					Team
+				</H1>
+				<Text>
+					{this.props.user.username}
+				</Text>
 				<Card>
 					<CardItem>
-						<Text>ADDJSHDJSNDLKNSDkSND</Text>
+						<Text>Almond Lima</Text>
+					</CardItem>
+					<CardItem>
+						<Text>Team Name:</Text>
+						<Input placeholder="team name" />
+						<Button><Text>Create Team</Text></Button>
 					</CardItem>
 				</Card>
+
+				<Card>
+					<CardItem header bordered>
+						<Text>
+							My Teammates
+						</Text>
+					</CardItem>
+					<CardItem>
+						<Content>
+							<List>
+								<TeamListItem />
+								<TeamListItem />
+							</List>
+						</Content>
+					</CardItem>
+				</Card>
+				<Card>
+					<CardItem header bordered>
+						<Text>
+							Search for Player
+						</Text>
+					</CardItem>
+					<CardItem>
+						<Content>
+							<List>
+								<TeamListItem addToTeam={true} />
+								<TeamListItem addToTeam={true} />
+							</List>
+						</Content>
+					</CardItem>
+				</Card>
+				<Button onPress={() => this.props.navigation.navigate('Main')}><Text>Back</Text></Button>
 			</Content>
 		);
 	}
-} 
+}
+
+
+const mapStateToProps = state => {
+	return {
+		user: state.user
+	}
+}
+
+// const mapDispatchToProps = dispatch => {
+
+// }
+
+export default connect(mapStateToProps, null)(TeamScreen)
