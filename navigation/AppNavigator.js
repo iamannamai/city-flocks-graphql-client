d9d4cdab96116498dafca963709bfd7c289714a8
AbuSufianIslam <thebooming1992@gmail.com>
2019-05-09 17:01:44 -0400

    adding task page

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -5,19 +5,21 @@ import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import AuthNavigator from '../screens/SignInScreen';
 import UserScreen from '../screens/UserScreen';
 import EventsScreen from '../screens/EventsScreen';
+import TaskScreen from '../screens/TaskScreen';
 
 export default createAppContainer(
-  createSwitchNavigator(
-    {
-      // You could add another route here for authentication.
-      // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-      AuthLoading: AuthLoadingScreen,
-      Auth: AuthNavigator,
-      Main: UserScreen,
-      Events: EventsScreen
-    },
-    {
-      initialRouteName: "AuthLoading"
-    }
-  )
+	createSwitchNavigator(
+		{
+			// You could add another route here for authentication.
+			// Read more at https://reactnavigation.org/docs/en/auth-flow.html
+			AuthLoading: AuthLoadingScreen,
+			Auth: AuthNavigator,
+			Main: UserScreen,
+			Events: EventsScreen,
+			Task: TaskScreen
+		},
+		{
+			initialRouteName: 'AuthLoading'
+		}
+	)
 );
