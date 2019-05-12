b5eba10bfa65902e3dbabd8b8339c96e9f023626
juanmarges <46904102+juanmarges@users.noreply.github.com>
2019-05-11 21:47:43 -0400

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'http://city-flocks-server.herokuapp.com';
-export const BASE_URL = 'https://f850d487.ngrok.io';
+export const BASE_URL = 'http://city-flocks-server.herokuapp.com';
