c17e55d54be5377406ff63b3af262b0aadb73cdd
juanmarges <46904102+juanmarges@users.noreply.github.com>
2019-05-17 11:41:38 -0400

    Update constants.js
    switch back to heroku

--- constants/constants.js
+++ constants/constants.js
@@ -1,2 +1,1 @@
-//export const BASE_URL = 'http://city-flocks-server.herokuapp.com';
-export const BASE_URL = 'https://e12930fe.ngrok.io';
+export const BASE_URL = 'http://city-flocks-server.herokuapp.com';
